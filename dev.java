6369c18522f58902eb4f6ace5016efde77e6a751
kvr-007 <135876591+kvr-007@users.noreply.github.com>
2023-06-20 11:44:58 -0500

    Update dev.java

--- dev.java
+++ dev.java
@@ -4,4 +4,4 @@ this is something for test purpose
 this is something for test purpose
 this is something for test purpose
 this is something for test purpose
-i dont know how the error resolved and suddenly build is working fine 
+Seems like now the build is success and its not needed to declare any java_home in jenkins i guess. still its mysterious.
